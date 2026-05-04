526f819abc6c2b0201f45211f327076ad5522124
Akshay Yadav <akshayyadav12356@gmail.com>
2026-05-04 15:23:59 +0530

    cors issue solved

--- backend/server.js
+++ backend/server.js
@@ -16,8 +16,20 @@ const settingsRoutes = require("./src/routes/settingsRoutes");
 const app = express();
 
 // Middleware
+const allowedOrigins = [
+  process.env.FRONTEND_URL,
+  "http://localhost:5173",
+  "http://127.0.0.1:5173"
+];
+
 app.use(cors({
-  origin: process.env.FRONTEND_URL,
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   credentials: true
 }));
 app.use(express.json());
